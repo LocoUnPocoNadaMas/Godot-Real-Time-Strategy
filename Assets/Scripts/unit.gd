6d6466ed9c3e452d97e4207da347075fe004b987
Pato <dkno_c1@hotmail.com>
2024-03-15 03:56:11 -0300

    added somethings to controller but not work for now

--- Assets/Scripts/unit.gd
+++ Assets/Scripts/unit.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var _damage: int = 20
 
 @export_group("Attack")
-@export var _move_speed: float = 50
+@export var _move_speed: float = 500
 @export var _attack_range: float = 20
 @export var _attack_rate: float = 0.5
 
@@ -20,7 +20,17 @@ var _sprite: Sprite2D
 func _ready():
 	_agent = get_node("NavigationAgent2D")
 	_sprite = get_node("Sprite2D")
+	_move_to_location(Vector2(100,0))
 
+
+func _physics_process(delta):
+	_target_check()
+	if _agent.is_navigation_finished():
+		return
+	var dir = global_position.direction_to(_agent.get_next_path_position())
+	velocity = dir * _move_speed * delta
+	move_and_slide()
+	
 
 func take_damage(damage: int) -> void:
 	_health -= damage
@@ -35,10 +45,20 @@ func _try_attack_target() -> void:
 		_last_attack = current_time
 
 
-func set_target(new_target: Unit) -> void:
+func _set_target(new_target: Unit) -> void:
 	_target = new_target
 
 
-func move_to_location(location: Vector2) -> void:
+func _move_to_location(location: Vector2) -> void:
 	_target = null
 	_agent.target_position = location
+
+
+func _target_check() -> void:
+	if(_target != null):
+		var dist = global_position.direction_to(_target.global_position)
+		if(dist <= _attack_range):
+			_agent.target_position = global_position
+			_try_attack_target()
+		else:
+			_agent.target_position = _target.global_position
